ff636fe977c99292f6cd65d686e35948143ec0a5
Atulaman <atul.aman@remotestate.com>
2024-07-24 19:21:26 +0530

    Revert "added backup script"
    This reverts commit f1a5f68798a96d23e8047b90423f20f3ee99fb65.

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source_dir="/home/parvej/demo/source"
-backup_dir="/home/parvej/demo/backup"
-log_file="/home/parvej/demo/backup.log"
-mkdir -p "$backup_dir"
-rsync -av --delete "$source_dir" "$backup_dir" > "log_file" 2>&1
-echo "backup completed on $(date +%d-%m-%y)"
-cat /home/parvej/demo/backup.log
